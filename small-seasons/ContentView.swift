eca38be6d7e3e155cf5cb2fc5a504334f6686a33
Helen V. Holmes <helen.v.holmes@gmail.com>
2023-11-19 15:53:55 -0500

    Initial Commit

---
+++ small-seasons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  small-seasons
+//
+//  Created by Sherlock Holmes on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ small-seasons/small_seasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  small_seasonsApp.swift
+//  small-seasons
+//
+//  Created by Sherlock Holmes on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct small_seasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
